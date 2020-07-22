279ffb411238657f7b7a4873615ff0d0dedb89a9
Sante Altamura <37416025+santealtamura97@users.noreply.github.com>
2020-07-22 09:29:07 +0200

    Update iterativeDeepening.pl

--- iterativeDeepening.pl
+++ iterativeDeepening.pl
@@ -1,9 +1,4 @@
-% Profondità con una profondità limitata
 
-%predicato dinamico
-%l'1 rappresenta il numero di argomenti
-%rappresenta una variabile globale che aggiungo al database
-%durante l'esecuzione del programma
 :-dynamic(limite/1).
 limite(1).
 
@@ -14,20 +9,13 @@ playid:-
    write('Execution took '), write(ExecutionTime), write(' ms.'), nl.
 
 
-%Inizialmente l'interprete prolog prende in considerazione
-%la prima clausola a cui si da il parametro di output "Soluzione"
-%poi considera e cerca di dimostrare il predicato ricercaPath.
-%dopo c'è un cut in modo tale da rendere le scelte definitive, 
-%cioè, se restituisce una soluzione per ricercaPath allora 
-%ignora tutte l'altra possibile scelte in cui aumenta di 1 il limite
 iterativeDeepening(Soluzione):-
     ricercaPath(Soluzione),
     !,
     retract(limite(_)),
     assert(limite(1)).
  
-%effettua una normalissima ricerca in profondità limitata dal
-%limite corrente
+
 ricercaPath(Soluzione):-
     limite(N),
     Limite is N,
@@ -35,11 +23,7 @@ ricercaPath(Soluzione):-
     iniziale(S), 
     ricerca_depth_limitata(S,[],Limite,Soluzione).
 
-%se la ricercaPath di sopra non è andata a buonafine,
-%cioè l'interprete prolog non è riuscito a trovare uno stato finale
-%in base al limite corrente
-%viene presa in considerazione questa seconda scelta e aumentato il limite
-%di 1
+
 ricercaPath(Soluzione):-
     limite(N),
     N1 is N+1,
