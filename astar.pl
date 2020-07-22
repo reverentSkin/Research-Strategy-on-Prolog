358bdb646aaeb0b46e18d7fc3a7efc319a89ede6
Sante Altamura <37416025+santealtamura97@users.noreply.github.com>
2020-07-22 09:27:46 +0200

    Update astar.pl

--- astar.pl
+++ astar.pl
@@ -12,28 +12,18 @@ playa:-
     statistics(walltime, [_ | [ExecutionTime]]),
     write('Execution took '), write(ExecutionTime), write(' ms.'), nl.
 
-/*
-come nella ricerca in ampiezza abbiamo la necessità di tener traccia
-delle azioni per arrivare ad uno stato S, del suo costo per arrivarci,
-e dell'euristica rispetto allo stato finale
-*/
+
 astar(Soluzione):-
     iniziale(S),
     heuristic(S,HeuristicS),
     astarRicerca([nodo(S,[],0,HeuristicS)],[],Soluzione),
     !.
 
-/*Se ci troviamo nello stato finale, le azioni da restituire
-come parametro di output
-sono proprio le azioni per S*/
+
 astarRicerca([nodo(S,AzioniPerS,_,_)|_],_,AzioniPerS):-
     finale(S),!.
 
-/*avviene una ricerca in ampiezza, solo che a differenza di quella
-standard dove alla chiamata ricorsiva si passerebbe NuovaCoda
-qui viene passata CodaOrdinata secondo le F(S) degli stati
-dai nodi che ce l'hanno più piccola a quelli che ce l'hanno
-più grande*/
+
 astarRicerca([nodo(S,AzioniPerS,CostoAttuale,HeuristicAttuale)|CodaStati],Visitati,Soluzione):-
     findall(Az,applicabile(Az,S),ListaAzioniApplicabili),
     generaStatiFigli(nodo(S,AzioniPerS,CostoAttuale,HeuristicAttuale),[S|Visitati],ListaAzioniApplicabili,StatiFigli),
@@ -41,13 +31,10 @@ astarRicerca([nodo(S,AzioniPerS,CostoAttuale,HeuristicAttuale)|CodaStati],Visita
     predsort(a_star_comparator,NuovaCoda,CodaOrdinata),
     astarRicerca(CodaOrdinata,[S|Visitati],Soluzione).
 
-/*Caso base: la lista azioni applicabili è vuota
-allora la lista di stati figli è vuota*/
+
 generaStatiFigli(_,_,[],[]).
 
-/*Nel caso induttivo, ad ogni chiamata viene messo in testa
-alla lista dei figli il nodo rappresentato dal costo del suo percorso
-e dal valore dell'euristica*/
+
 generaStatiFigli(nodo(S,AzioniPerS,CostoAttuale,HeuristicAttuale),Visitati,[Az|AltreAzioni],
                 [nodo(SNuovo,[Az|AzioniPerS],NuovoCostoPerS,NuovaEuristicaPerS)|AltriFigli]):-
     trasforma(Az,S,SNuovo),
@@ -58,7 +45,6 @@ generaStatiFigli(nodo(S,AzioniPerS,CostoAttuale,HeuristicAttuale),Visitati,[Az|A
     heuristic(SNuovo,NuovaEuristicaPerS),
     generaStatiFigli(nodo(S,AzioniPerS,CostoAttuale,HeuristicAttuale),[SNuovo|Visitati],AltreAzioni,AltriFigli).
 
-%fa riferimento al caso in cui SNuovo è gia stato visitato
-%ma dobbiamo proseguire
+
 generaStatiFigli(Nodo,Visitati,[_|AltreAzioni],AltriFigli):-
     generaStatiFigli(Nodo,Visitati,AltreAzioni,AltriFigli).
