399ffd6c36930497ee9bc01683de52b2e9f403f8
Sante Altamura <37416025+santealtamura97@users.noreply.github.com>
2020-07-22 09:26:58 +0200

    Add files via upload

---
+++ Maps/3X3_A.pl
@@ -0,0 +1,9 @@
+num_righe(3).
+num_colonne(3).
+
+iniziale(pos(1,1)).
+finale(pos(3,3)).
+
+occupata(pos(1,2)).
+occupata(pos(2,2)).
+occupata(pos(2,1)).
---
+++ Maps/5X5_A.pl
@@ -0,0 +1,14 @@
+num_righe(5).
+num_colonne(5).
+
+
+iniziale(pos(2,2)).
+finale(pos(5,4)).
+
+occupata(pos(1,1)).
+occupata(pos(1,2)).
+
+occupata(pos(2,4)).
+
+occupata(pos(3,2)).
+occupata(pos(3,3)).
